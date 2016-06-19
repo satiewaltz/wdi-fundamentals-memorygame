2727a8c4fd4acbf8b29fc47681b5be59eaf96672
satiewaltz <davebarthly@gmail.com>
2016-06-19 19:29:34 -0400

    Working Memory Game! Wrapped everything in functions

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,33 +1,61 @@
-console.log("JS file is connected to HTML! Woo!")
+console.log("JS file is connected to HTML! Woo!");
 
-var cardOne = "queen";
+// == Only one global variable :D == //
+var cardsInPlay = [];
 
-var cardTwo = "king"
-
-var cardThree = "queen"
-
-var cardFour = "king"
-
-// if (cardTwo === cardFour) {
-// 	alert("You've found a match!");
-// } else {
-// 	alert("Sorry, try again.");
-// }
-
-// if (cardOne === cardThree) {
-// 	alert("You've found a match!");
-// } else {
-// 	alert("Sorry, try again.");
-// }
-
-var board = document.getElementById('game-board');
-
-function createBoard () {
-	for (var i = 0; i < 4; i++) {
-		var newCard = document.createElement('div')
-		newCard.className = 'card';
-		board.appendChild(newCard);
+// == Preloader to fix first turn bug == //
+function preload() {
+	var images = [];
+	for (var i = 0; i < arguments.length; i++) {
+		images[i] = new Image();
+		images[i].src = preload.arguments[i];
 	}
 }
 
-createBoard();
+// == Create and Initialize Board == //
+(function createBoard () {
+	preload("king.png","queen.png");
+	var cards = ["queen", "queen", "king", "king"];
+	var board = document.getElementById('game-board');
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('div');
+		cardElement.setAttribute('data-card', cards[i]);
+		cardElement.addEventListener('click', isTwoCards);
+		cardElement.className = 'card';
+		board.appendChild(cardElement);
+	}
+})();
+
+// == Card Checker == //
+function isTwoCards() {
+	cardsInPlay.push(this.getAttribute('data-card'));
+
+	if (this.getAttribute('data-card') == "king") {
+		this.innerHTML = '<img src="king.png" alt="King of Spades"/>';
+	} else {
+		this.innerHTML = '<img src="queen.png" alt="Queen of Spades"/>';
+	}
+
+	if (cardsInPlay.length === 2) {
+		isMatch(cardsInPlay);
+		imageRemover();
+		cardsInPlay = [];
+	}
+}
+
+// == Card Matcher == //
+function isMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You've found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
+
+// == Image Remover Function for clearing board == //
+function imageRemover () {
+	var images = document.getElementsByTagName("img");
+	while(images.length > 0) {
+		images[0].parentNode.removeChild(images[0]);
+	}
+}
